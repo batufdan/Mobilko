adebb3854990875f466465efa5abe12bd9a4847d
kirciduru <kirciduru@gmail.com>
2024-12-23 19:24:54 +0300

    olacak inşallah

--- app/src/main/java/com/zeynepturk/project_487/HomePageActivity.kt
+++ app/src/main/java/com/zeynepturk/project_487/HomePageActivity.kt
@@ -2,34 +2,26 @@ package com.zeynepturk.project_487
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.room.Room
 import com.zeynepturk.project_487.databinding.ActivityHomePageBinding
 import com.zeynepturk.project_487.db.MobilkoRoomDatabase
 import com.zeynepturk.project_487.model.Admin
 import com.zeynepturk.project_487.model.Student
+import com.zeynepturk.project_487.retrofit.StudentService
+import com.zeynepturk.project_487.util.ApiClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.Collections
 
 class HomePageActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomePageBinding
-    private val admins = arrayListOf(
-        Admin(1, "duruko16", "Duru Kırcı"),
-        Admin(2, "zeyno1", "Zeynep Türk"),
-        Admin(3, "1111", "Batuhan Fidan"),
-        Admin(4, "sueda55", "Sueda Akça")
-    )
-
-    private val students = arrayListOf(
-        Student(1, "duruko", "Duru Kırcı", "kirciduru@gmail.com", 4.00),
-        Student(2, "zeyno", "Zeynep Türk", "zeynepturk74@gmail.com", 3.98),
-        Student(3, "1111", "Batuhan Fidan", "batuhanfidan59@gmail.com", 1.80),
-        Student(4, "sueda", "Sueda Akça","akcasueda@gmail.com", 3.96)
-    )
-
+    lateinit var students: ArrayList<Student>
+    lateinit var admins:  ArrayList<Admin>
+    lateinit var studentService: StudentService
     private val mobilkoDB: MobilkoRoomDatabase by lazy {
         Room.databaseBuilder(this, MobilkoRoomDatabase::class.java, "MobilkoDB")
             .allowMainThreadQueries()
@@ -42,6 +34,8 @@ class HomePageActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityHomePageBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        studentService = ApiClient.getClient()
+            .create(StudentService::class.java)
 
         getData()
 
@@ -82,7 +76,7 @@ class HomePageActivity : AppCompatActivity() {
 
             if (foundStudent != null) {
                 showToast("Welcome, ${foundStudent.name}!")
-                val intent = Intent(this, MainActivity::class.java).apply {
+                val intent = Intent(this, StudentMainActivity::class.java).apply {
                     putExtra("STUDENT_ID", enteredId)
                 }
                 startActivity(intent)
@@ -97,10 +91,33 @@ class HomePageActivity : AppCompatActivity() {
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
+    fun prepareData() {
+
+        val request = studentService.getStudents()
+        request.enqueue(object : Callback<ArrayList<Student>> {
+            override fun onFailure(call: Call<ArrayList<Student>>, t: Throwable) {
+                Toast.makeText(applicationContext, t.message.toString(), Toast.LENGTH_LONG).show()
+            }
+            override fun onResponse(call: Call<ArrayList<Student>>, response: Response<ArrayList<Student>>) {
+                if (response.isSuccessful) {
+                    students = (response.body() as ArrayList<Student>?)!!
+                }
+            }
+        })
+
+        Collections.addAll(
+            admins,
+            Admin(1, "duruko16", "Duru Kırcı"),
+            Admin(2, "zeyno1", "Zeynep Türk"),
+            Admin(3, "1111", "Batuhan Fidan"),
+            Admin(4, "sueda55", "Sueda Akça")
+        )
+    }
+
 
     private fun getData(){
+        prepareData()
         mobilkoDB.studentDao().insertAllStudent(students)
         mobilkoDB.adminDao().insertAllAdmin(admins)
-
     }
 }
