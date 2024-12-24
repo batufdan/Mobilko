f43cbd0c1564eb706a8ef381e61ac3293231f803
suedakca <akcasueda@gmail.com>
2024-12-24 14:56:39 +0300

    office hour

--- app/src/main/java/com/zeynepturk/project_487/StudentMainActivity.kt
+++ app/src/main/java/com/zeynepturk/project_487/StudentMainActivity.kt
@@ -1,14 +1,44 @@
 package com.zeynepturk.project_487
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.room.Room
+import com.zeynepturk.project_487.databinding.ActivityStuMainBinding
+import com.zeynepturk.project_487.db.MobilkoRoomDatabase
 
 class StudentMainActivity : AppCompatActivity() {
+    lateinit var bindingStu: ActivityStuMainBinding
+    private val mobilkoDB: MobilkoRoomDatabase by lazy {
+        Room.databaseBuilder(this, MobilkoRoomDatabase::class.java, "MobilkoDB")
+            .allowMainThreadQueries()
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_stu_main)
+        bindingStu = ActivityStuMainBinding.inflate(layoutInflater)
+        setContentView(bindingStu.root)
+
+        val b = intent.extras
+        val stuID = b?.getInt("STUDENT_ID")
+        val stu = mobilkoDB.studentDao().getStudentById(stuID!!)
+
+        bindingStu.stuID.text = stuID.toString()
+        bindingStu.stuCGPA.text = stu.cgpa.toString()
+        bindingStu.stuMail.text = stu.mail
+        bindingStu.stuName.text = stu.name
+
+
+        bindingStu.officeHourButton.setOnClickListener {
+            val intent = Intent(this, OfficeHourActivity::class.java)
+            intent.putExtra("Student_Id", stuID)
+            startActivity(intent)
+        }
 
     }
 }
---
+++ app/src/main/java/com/zeynepturk/project_487/adapter/CustomStudentRVAdapter.kt
@@ -0,0 +1,40 @@
+package com.zeynepturk.project_487.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.zeynepturk.project_487.R
+import com.zeynepturk.project_487.model.Courses
+import com.zeynepturk.project_487.model.Student
+
+class CustomStudentRVAdapter(private val context: Context, private val onItemClicked: (Courses) -> Unit) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    var recyclerViewItem = emptyList<Courses>()
+
+    fun setData(items: MutableList<Courses>) {
+        recyclerViewItem = items
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflator = LayoutInflater.from(parent.context)
+        val itemView: View = inflator.inflate(R.layout.item_stu_course, parent, false)
+        return CourseViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return recyclerViewItem.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    inner class CourseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val courseCode: TextView = itemView.findViewById(R.id.courseCodeTxt)
+        val courseName: TextView = itemView.findViewById(R.id.courseNameTxt)
+    }
+}
